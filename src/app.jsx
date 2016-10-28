d666590ab6e046ba6c3ba733696c020007ced7b0
Michael Awty <mc.awty@gmail.com>
2016-10-28 19:33:26 +0100

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+require("./scss/style.scss");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/hello-world.jsx';
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('root')
+);
